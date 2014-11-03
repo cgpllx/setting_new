736f002700f76c97daedf655769dba579c314b3d
cgpllx <cgpllx1@qq.com>
2014-11-03 21:42:34 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

--- src/com/kubeiwu/commontool/view/setting/ListRowView.java
+++ src/com/kubeiwu/commontool/view/setting/ListRowView.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.preference.ListPreference;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.View;
