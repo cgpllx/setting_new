1bb76c22a6125d9a39cfef56957c9ea9a8ab356c
Administrator <Administrator@PC-20150408YMYE>
2015-12-01 09:23:39 +0800

    Signed-off-by: Administrator <Administrator@PC-20150408YMYE>

--- src/com/kubeiwu/commontool/view/setting/KSettingView.java
+++ src/com/kubeiwu/commontool/view/setting/KSettingView.java
@@ -13,139 +13,138 @@ import com.kubeiwu.commontool.view.config.GroupViewConfig;
 
 public class KSettingView extends LinearLayout {
 
-	public KSettingView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		initView();
-	}
+    public KSettingView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initView();
+    }
 
-	private void initView() {
-		setOrientation(VERTICAL);
-		setPadding(00, 10, 00, 10);
-		LayoutParams l = new LayoutParams(android.view.ViewGroup.LayoutParams.MATCH_PARENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
-		// l.setMargins(50, 10, 20, 10);
-		setLayoutParams(l);
+    private void initView() {
+        setOrientation(VERTICAL);
+        setPadding(00, 10, 00, 10);
+        LayoutParams l = new LayoutParams(android.view.ViewGroup.LayoutParams.MATCH_PARENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
+        // l.setMargins(50, 10, 20, 10);
+        setLayoutParams(l);
 
-	}
+    }
 
-	public KSettingView(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
+    public KSettingView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
-	public KSettingView(Context context) {
-		super(context);
-		initView();
-	}
+    public KSettingView(Context context) {
+        super(context);
+        initView();
+    }
 
-	/**
-	 * 这里会传到groupView中
-	 */
-	private DisplayOptions displayOptions;
-	private GroupViewConfig itemBgSelectorUtil;
+    /**
+     * 这里会传到groupView中
+     */
+    private DisplayOptions displayOptions;
+    private GroupViewConfig itemBgSelectorUtil;
 
-	public void setDisplayOptions(DisplayOptions displayOptions) {
-		this.displayOptions = displayOptions;
-	}
+    public void setDisplayOptions(DisplayOptions displayOptions) {
+        this.displayOptions = displayOptions;
+    }
 
-	private SparseArray<GroupView> mGroupViewArray = new SparseArray<GroupView>();
+    private SparseArray<GroupView> mGroupViewArray = new SparseArray<GroupView>();
 
-	/**
-	 * 增加一个GroupView到容器中
-	 * 
-	 * @param groupOrder
-	 *            排序id
-	 * @param gorupViewTitle
-	 *            title
-	 * @return 返回被增加的GroupView
-	 */
-	public GroupView addGroupViewItem(int groupOrder, String gorupViewTitle) {
-		GroupView entry = mGroupViewArray.get(groupOrder);
-		GroupView groupView = new GroupView.Builder(getContext()).setGorupViewTitle(gorupViewTitle).create();
+    /**
+     * 增加一个GroupView到容器中
+     *
+     * @param groupOrder     排序id
+     * @param gorupViewTitle title
+     * @return 返回被增加的GroupView
+     */
+    public GroupView addGroupViewItem(int groupOrder, String gorupViewTitle) {
+        GroupView entry = mGroupViewArray.get(groupOrder);
+        GroupView groupView = new GroupView.Builder(getContext()).setGorupViewTitle(gorupViewTitle).create();
 
-		if (entry == null) {
-			mGroupViewArray.put(groupOrder, groupView);
-		} else {
-			entry.addGroupViewLastNode(groupView);
-		}
-		return groupView;
-	}
+        if (entry == null) {
+            mGroupViewArray.put(groupOrder, groupView);
+        } else {
+            entry.addGroupViewLastNode(groupView);
+        }
+        return groupView;
+    }
 
-	/**
-	 * 增加一个不需要title的GroupView到容器中
-	 * 
-	 * @param groupOrder
-	 * @return 返回被增加的GroupView
-	 */
-	public GroupView addGroupViewItem(int groupOrder) {
-		return addGroupViewItem(groupOrder, null);
-	}
+    /**
+     * 增加一个不需要title的GroupView到容器中
+     *
+     * @param groupOrder
+     * @return 返回被增加的GroupView
+     */
+    public GroupView addGroupViewItem(int groupOrder) {
+        return addGroupViewItem(groupOrder, null);
+    }
 
-	// 默认时候的组id
-	private int groupOrder;
+    // 默认时候的组id
+    private int groupOrder;
 
-	/**
-	 * 增加一个不需要title的GroupView到容器中
-	 * 
-	 * @param groupOrder
-	 * @return 返回被增加的GroupView
-	 */
-	public GroupView addGroupViewItem() {
+    /**
+     * 增加一个不需要title的GroupView到容器中
+     *
+     * @param groupOrder
+     * @return 返回被增加的GroupView
+     */
+    public GroupView addGroupViewItem() {
 
-		return addGroupViewItem(groupOrder++, null);
-	}
+        return addGroupViewItem(groupOrder++, null);
+    }
 
-	/**
-	 * 向容器中增加一个GroupView，如果groupOrder位置 在容器中已经存在，则会在下面增加
-	 * 
-	 * @param groupViewArray
-	 */
-	public void addGroupView(int groupOrder, GroupView groupView) {
-		GroupView delivery = this.mGroupViewArray.get(groupOrder);
-		if (delivery == null) {
-			this.mGroupViewArray.put(groupOrder, groupView);
-		} else {
-			delivery.addGroupViewLastNode(groupView);
-		}
-	}
+    /**
+     * 向容器中增加一个GroupView，如果groupOrder位置 在容器中已经存在，则会在下面增加
+     *
+     * @param groupViewArray
+     */
+    public void addGroupView(int groupOrder, GroupView groupView) {
+        GroupView delivery = this.mGroupViewArray.get(groupOrder);
+        if (delivery == null) {
+            this.mGroupViewArray.put(groupOrder, groupView);
+        } else {
+            delivery.addGroupViewLastNode(groupView);
+        }
+    }
 
-	public void commit() {
-		notifyDataChanged();
-	}
+    public void commit() {
+        notifyDataChanged();
+    }
 
-	// 重载addview
-	public void addView(GroupView child) {
-		String title = child.getTitle();
-		if (!TextUtils.isEmpty(title)) {
-			TextView textview = new TextView(getContext());
-			textview.setTextSize(getDisplayOptions().getUnit(),getDisplayOptions().getGroupTitleSize());
-			textview.setTextColor(getResources().getColor(getDisplayOptions().getGroupTitleColorId()));
-			textview.setText(title);
-			addView(textview);
-		}
-		super.addView(child);
-		child.notifyDataChanged(itemBgSelectorUtil);
-	}
+    // 重载addview
+    public void addView(GroupView child) {
+        String title = child.getTitle();
+        if (!TextUtils.isEmpty(title)) {
+            TextView textview = new TextView(getContext());
+            textview.setTextSize(getDisplayOptions().getUnit(), getDisplayOptions().getGroupTitleSize());
+            textview.setTextColor(getResources().getColor(getDisplayOptions().getGroupTitleColorId()));
+            textview.setPadding(10, 30, 0, 30);
+            textview.setText(title);
+            addView(textview);
+        }
+        super.addView(child);
+        child.notifyDataChanged(itemBgSelectorUtil);
+    }
 
-	public DisplayOptions getDisplayOptions() {
-		if (displayOptions == null) {
-			displayOptions = DisplayOptions.createsimpleDisplayOptions();
-		}
-		return displayOptions;
-	}
+    public DisplayOptions getDisplayOptions() {
+        if (displayOptions == null) {
+            displayOptions = DisplayOptions.createsimpleDisplayOptions();
+        }
+        return displayOptions;
+    }
 
-	public void notifyDataChanged() {
-		itemBgSelectorUtil=new GroupViewConfig(getDisplayOptions());
-		if (mGroupViewArray != null && mGroupViewArray.size() > 0) {
-			GroupView groupView = null;
-			for (int i = 0; i < mGroupViewArray.size(); i++) {
-				groupView = mGroupViewArray.valueAt(i);
-				addView(groupView);
-				while (groupView.hasNext()) {
-					groupView = groupView.getNext();
-					addView(groupView);
-				}
-			}
-		} else {
-			setVisibility(View.GONE);
-		}
-	}
+    public void notifyDataChanged() {
+        itemBgSelectorUtil = new GroupViewConfig(getDisplayOptions());
+        if (mGroupViewArray != null && mGroupViewArray.size() > 0) {
+            GroupView groupView = null;
+            for (int i = 0; i < mGroupViewArray.size(); i++) {
+                groupView = mGroupViewArray.valueAt(i);
+                addView(groupView);
+                while (groupView.hasNext()) {
+                    groupView = groupView.getNext();
+                    addView(groupView);
+                }
+            }
+        } else {
+            setVisibility(View.GONE);
+        }
+    }
 }
